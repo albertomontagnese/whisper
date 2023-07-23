b8ef2c8968b61a3711a799dc2e0610f6dbced15c
Alberto <alberto.montagnese@gmail.com>
2023-07-22 17:55:07 -0700

    test

---
+++ .firebase/whisperingapp/functions/next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+var nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
+  }
+};
+module.exports = nextConfig;
